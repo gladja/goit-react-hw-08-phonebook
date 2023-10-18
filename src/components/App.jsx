099140e0ebb9d13720be3422f8091b38d396ec26
gladja <gladkih.alexander@gmail.com>
2023-10-18 23:02:28 +0300

    creat route and navlink

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { Register } from '../pages/Register';
+import { Login } from '../pages/Login';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <ul>
+        <li>
+          <NavLink to={'/'}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to={'/register'}>Register</NavLink>
+        </li>
+        <li>
+          <NavLink to={'/login'}>Login</NavLink>
+        </li>
+        <li>
+          <NavLink to={'/contacts'}>Contacts</NavLink>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/contacts' element={'Contacts'} />
+      </Routes>
+    </>
   );
 };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+  return (
+    <>
+    <h1>Say hello to your phone book!</h1>
+      <h2>Please register or log in with an account</h2>
+    </>
+  )
+}
